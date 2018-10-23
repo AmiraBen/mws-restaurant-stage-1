c3f14a93b14959fae9ac0c7fba113d070eb84ac4
Amira Bennani <amirabennani1955m@gmail.com>
2018-10-23 21:15:44 +0100

    Finish the mobile first version css.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYW1pcmFiZW4iLCJhIjoiY2puY25icThpMDR1NDNxbGZxYjZmaHNyMiJ9.jiHLG2AtfgFPcuiOvdsAZw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
